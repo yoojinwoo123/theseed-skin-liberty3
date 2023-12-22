ae2fc5c0165e289bbaa042ae3143624a6c1b0a54
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2023-12-22 18:08:06 +0900

    Fix not displayed error title

--- layout.vue
+++ layout.vue
@@ -155,7 +155,7 @@
                         </div>
                     </div>
                     <div class="title">
-                        <h1 v-if="$store.state.page.data.document">
+                        <h1 v-if="$store.state.page.data.document && $store.state.page.viewName !== 'error'">
                             <nuxt-link :to="doc_action_link($store.state.page.data.document, 'w')"><span v-if="$store.state.page.data.document.forceShowNamespace !== false" class="namespace">{{$store.state.page.data.document.namespace}}:</span>{{$store.state.page.data.document.title}}</nuxt-link>
                             <small v-if="$store.state.page.viewName === 'edit_edit_request' || $store.state.page.viewName === 'edit_request'">(편집 요청)</small>
                             <small v-else-if="$store.state.page.viewName === 'edit' && $store.state.page.data.body.section">(r{{$store.state.page.data.body.baserev}} 문단 편집)</small>
