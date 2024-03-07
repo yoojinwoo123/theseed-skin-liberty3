640ab74318df9a452b3df01d7fb7da903354bc58
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2024-03-08 01:31:30 +0900

    Fix recentCard local-date format

--- components/recentCard.vue
+++ components/recentCard.vue
@@ -14,7 +14,9 @@
                 <li><span class="recent-item">&nbsp;</span></li>
             </template>
             <template v-else>
-                <li v-for="(r,i) in recent" v-bind:key="i"><nuxt-link class="recent-item" v-bind:key="r.document" :to="doc_action_link(r.document, 'w')">[<local-date :date="r.date" format="H:i:s" />] {{ r.document}}</nuxt-link></li>
+                <li v-for="(r,i) in recent" :key="i">
+                    <nuxt-link class="recent-item" :key="r.document" :to="doc_action_link(r.document, 'w')">[<local-date :date="r.date" :format="getDateType(r.date)" />] {{ r.document }}</nuxt-link>
+                </li>
             </template>
         </ul>
     </div>
@@ -23,6 +25,12 @@
 <script>
 import RecentCardMixin from '~/mixins/recentCard';
 export default {
-    mixins: [RecentCardMixin]
+    mixins: [RecentCardMixin],
+    methods: {
+        getDateType(date) {
+            const now = Math.floor((new Date()).getTime() / 1000);
+            return (now - 86400) > date ? 'Y-m-d' : 'H:i:s';
+        }
+    }
 }
 </script>
